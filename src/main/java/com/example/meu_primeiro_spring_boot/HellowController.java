a461fd247dbdfbf7f65b79432d9deea7f16c6fb3
Agusto César <negocios.augusto@gmail.com>
2026-05-17 20:39:33 -0300

    Primeiro programa olá mundo criado com sucesso

---
+++ src/main/java/com/example/meu_primeiro_spring_boot/HellowController.java
@@ -0,0 +1,17 @@
+package com.example.meu_primeiro_spring_boot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
+public class HellowController {
+    
+    @GetMapping("/hello")
+    public String hello() {
+        return "Olá, mundo!! Bem-vindo ao Spring Boot";
+    }
+    
+}
